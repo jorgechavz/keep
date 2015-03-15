0966a44aabaf4b00fd7da296a97bf96aef750a11
Caio Lopes <caiodesalopes@gmail.com>
2015-03-15 02:12:35 -0700

    Add facebook login feature

---
+++ configuration/config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "facebook_api_key"      :     "1557769314499541",
+  "facebook_api_secret"   :     "dbbb0a61a668a3c38230fd574ee6709c",
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  "use_database"          :     "false",
+  "host"                  :     "localhost",
+  "username"              :     "root",
+  "password"              :     "123asd",
+  "database"              :     "keep"
+}
